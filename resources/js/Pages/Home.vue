f4211bb3281689beb9fa39704c55f03f66d67ef9
Tri Yulianto <yuliantotri851@gmail.com>
2024-06-18 15:24:04 +0700

    additional ui home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,6 +4,7 @@ import GroupList from '@/Components/app/GroupList.vue';
 import FollowingList from '@/Components/app/FollowingList.vue';
 import PostList from '@/Components/app/PostList.vue';
 import PostCreate from '@/Components/app/PostCreate.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 
 defineProps({
 
@@ -20,18 +21,21 @@ function handleImageError() {
 <template>
 
     <Head title="piu" />
-    <div class="grid lg:grid-cols-12 gap-3 p-3 h-full">
-        <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-            <GroupList />
+
+    <AuthenticatedLayout>
+        <div class="grid lg:grid-cols-12 gap-3 p-3 h-full">
+            <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+                <GroupList />
+            </div>
+            <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+                <FollowingList />
+            </div>
+            <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
+                <PostCreate />
+                <PostList class="flex-1" />
+            </div>
         </div>
-        <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
-            <FollowingList />
-        </div>
-        <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
-            <PostCreate />
-            <PostList class="flex-1" />
-        </div>
-    </div>
+    </AuthenticatedLayout>
 </template>
 
 <style>
