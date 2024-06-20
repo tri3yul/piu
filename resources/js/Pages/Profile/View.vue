86eaab1ffcf1c22efb97ffbf51470d9b53a3add5
Tri Yulianto <yuliantotri851@gmail.com>
2024-06-20 22:24:12 +0700

    fix edit profile user

--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -166,27 +166,12 @@ function submitAvatarImage () {
                         <h2 class="font-bold text-lg">
                             {{ user.name }}
                         </h2>
-                        <PrimaryButton v-if="isMyProfile">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                class="size-4 mr-2">
-                                <path
-                                    d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
-                                <path
-                                    d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
-                            </svg>
-
-
-                            Edit
-                        </PrimaryButton>
                     </div>
                 </div>
             </div>
             <div class="border-t">
                 <TabGroup>
                     <TabList class="flex bg-white">
-                        <Tab v-if="isMyProfile" v-slot="{ selected }" as="template">
-                            <TabItem text="About" :selected="selected" />
-                        </Tab>
                         <Tab v-slot="{ selected }" as="template">
                             <TabItem text="Post" :selected="selected" />
                         </Tab>
@@ -199,12 +184,12 @@ function submitAvatarImage () {
                         <Tab v-slot="{ selected }" as="template">
                             <TabItem text="Photo" :selected="selected" />
                         </Tab>
+                        <Tab v-if="isMyProfile" v-slot="{ selected }" as="template">
+                            <TabItem text="My Profile" :selected="selected" />
+                        </Tab>
                     </TabList>
 
                     <TabPanels class="mt-2">
-                        <TabPanel v-if="isMyProfile">
-                            <Edit :must-verify-email="mustVerifyEmail" :status="status" />
-                        </TabPanel>
                         <TabPanel class="bg-white p-3 shadow">
                             Post
                         </TabPanel>
@@ -216,6 +201,9 @@ function submitAvatarImage () {
                         </TabPanel>
                         <TabPanel class="bg-white p-3 shadow">
                             Photo
+                        </TabPanel>
+                        <TabPanel v-if="isMyProfile">
+                            <Edit :must-verify-email="mustVerifyEmail" :status="status" />
                         </TabPanel>
                     </TabPanels>
                 </TabGroup>
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ Route::get('/u/{user:username}', [ProfileController::class, 'index'])
 Route::middleware('auth')->group(function () {
     Route::post('/profile/update-image', [ProfileController::class, 'updateImage'])->name('profile.updateImages');
 //     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
