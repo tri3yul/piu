a8a4f37dda0b54b8dfcfbf73467a67c1fb904f92
Tri Yulianto <yuliantotri851@gmail.com>
2024-06-23 09:57:39 +0700

    add timezone

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
 
 class PostResource extends JsonResource
 {
@@ -17,8 +18,8 @@ class PostResource extends JsonResource
         return [
             'id' => $this->id,
             'body' => $this->body,
-            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
-            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'created_at' => Carbon::parse($this->created_at)->timezone('Asia/Jakarta')->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::parse($this->updated_at)->timezone('Asia/Jakarta')->format('Y-m-d H:i:s'),
             'user' => new UserResource($this->user),
             'group' => $this->group,
             'attachments' => $this->attachments,
