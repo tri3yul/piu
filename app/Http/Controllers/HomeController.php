875123bbf523d7532764f7193994125790d5d70b
Tri Yulianto <yuliantotri851@gmail.com>
2024-06-27 04:55:54 +0700

    add post like/unlike

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -11,7 +12,14 @@ class HomeController extends Controller
 {
     public function index (Request $request)
     {
-        $posts = Post::query()->latest()->paginate(20);
+        $userId = Auth::id();
+        $posts = Post::query()
+            ->withCount('reactions')
+            ->with(['reactions' => function ($query) use ($userId) {
+                $query->where('user_id', $userId);
+            }])
+            ->latest()
+            ->paginate(20);
         return Inertia::render('Home', [
             'posts' => PostResource::collection($posts)
         ]);
